c47c5c04fe3adc323b83da56cd379d085fd4e3f9
Steve <steve@thenightproject.com>
2026-09-11 11:46:21 +0100

    ⬆️ ruust 0.3.1 (TUI shell keybinding)

--- Casks/ruust.rb
+++ Casks/ruust.rb
@@ -1,23 +1,23 @@
 cask "ruust" do
-  version "0.3.0"
+  version "0.3.1"
 
   on_macos do
     on_arm do
-      sha256 "fcf15fa65bcbc412e1121577b796e68c4878a0b4e3ccea7da2d256269a9b6969"
+      sha256 "3ae24d2424dd2874e37925732c742015e8673fee95b7cce24a9555ea4164f071"
       url "https://github.com/RuustRun/cli/releases/download/v#{version}/ruust_#{version}_darwin_arm64.tar.gz"
     end
     on_intel do
-      sha256 "683f40946ed47e529523c15692baff9ddd695485883d54859c0bab29dae36a80"
+      sha256 "451aad843bb6af842a215f454d2a578ef891e1bc996304b91062ad6bfeb16e47"
       url "https://github.com/RuustRun/cli/releases/download/v#{version}/ruust_#{version}_darwin_amd64.tar.gz"
     end
   end
   on_linux do
     on_arm do
-      sha256 "42b0a19f36dbb8fa64dab4d324ffba52cfe7a79d0c01f0d981fa54c1019e23e9"
+      sha256 "54e6b02fa7dd5d2939b0db63f6db6a77a761ef2af0925b24c64a0e4e1f7ac538"
       url "https://github.com/RuustRun/cli/releases/download/v#{version}/ruust_#{version}_linux_arm64.tar.gz"
     end
     on_intel do
-      sha256 "41154562f4ef4ffd5c712d622b1d88bb6966faf14188893de1d8ce826a4e3e9f"
+      sha256 "36aa89fb51cc9fe3574f8f8c956a4b3eabee7d9b847aef5408d51eea2fccc67b"
       url "https://github.com/RuustRun/cli/releases/download/v#{version}/ruust_#{version}_linux_amd64.tar.gz"
     end
   end
